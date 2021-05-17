c73967c173f174f72ded3aa66b46ac6203279774
Otto Henrique <otto.henrique@emcasa.com>
2021-05-17 10:27:42 -0300

    documentação nos métodos da classe graph

--- src/lib/graph.rb
+++ src/lib/graph.rb
@@ -1,4 +1,16 @@
 class Graph
+  # salva os nós em um mapa usando o nome como chave e o custo como valor
+  # ex:
+  #   Graph.new(<<-NODES
+  #     A,B,10
+  #     B,c,20
+  #     NODES)
+  #
+  #  {
+  #    a: {b: 10}
+  #    b: {a: 10, c: 20}
+  #    c: {b: 20}
+  #  }
   def initialize(nodes)
     @nodes = {}
 
@@ -13,10 +25,14 @@ class Graph
     end
   end
 
+  # retorna todos os nós existentes no grafo
   def nodes
     @nodes.keys.sort
   end
 
+  # procura recursivamente todos os caminhos possíveis entre A e B
+  # retornando um array com todos os nós no caminho e o custo da jornada como
+  # último elemento.
   def paths(start, finish)
     trace(start, finish, [], 0).sort_by(&:size)
   end
